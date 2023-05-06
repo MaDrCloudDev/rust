7a6cb76818cfe44d58f1f495d0963d879b1fcdbc
MaDr <97172549+MaDrCloudDev@users.noreply.github.com>
2023-05-06 15:26:30 -0700

    init

---
+++ src/print.rs
@@ -0,0 +1,15 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print.rs file");
+
+    // Basic formatting
+    println!("{} is from {}", "Matt", "Arizona");
+
+    // Positional arguments
+    println!(
+        "{0} is from {1} and {0} likes to {2}",
+        "Matt", "Arizona", "code"
+    );
+
+    // Named arguments
+}
